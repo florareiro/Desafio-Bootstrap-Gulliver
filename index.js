b93a4275a46c1adfddd7242cd0b5769dad6e589b
florareiro <ceciliaflora@outlook.com.br>
2022-09-13 19:13:16 -0300

    api hospedagen

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening at 3000'));
+app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 
