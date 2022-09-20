f836f5c3968dddec1d7775d70cfae274245c0249
florareiro <ceciliaflora@outlook.com.br>
2022-09-19 21:18:00 -0300

    index hosp

--- public/main.js
+++ public/main.js
@@ -26,8 +26,26 @@ if ('geolocation' in navigator) {
     console.log('geolocalização disponivel');
     lat = position.coords.latitude;
     lon = position.coords.longitude;
+    
     const data = {lat, lon};
     
+
+
+
+
+   
+    // API CLIMA
+    const API_key = '1e37be63938b4de6be125501220709';
+   const api_url = `http://api.weatherapi.com/v1/current.json?key=${API_key}&q=${lat},${lon}&aqi=no&lo`;
+    const response = await fetch(api_url);
+    const json = await response.json();
+    console.log(json);
+    document.getElementById('cidade').textContent = json.location.name;
+    document.getElementById('regiao').textContent = json.location.region;
+    // document.getElementById('temperatura').textContent = json.current.temp_c;
+
+
+
   // API HOSPEDAGEM
     const key_api = 'dc0b091820824302a18c4037fc870ce1';
     const hospedagem_api = `https://api.geoapify.com/v2/places?categories=accommodation&filter=circle:${lon},${lat},5000&bias=proximity:${lon},${lat}&limit=50&apiKey=${key_api}`;
@@ -40,6 +58,8 @@ if ('geolocation' in navigator) {
      var hospedagens = hospedagem_json.features;
     //  console.log(hospedagens);
       
+
+
       var nome_hosp = L.geoJSON (hospedagens, {
         onEachFeature: function (Feature, nome_hosp){
          nome_hosp.bindPopup('<h2>'+ Feature.properties.name +'</h2>' + '</br>' + Feature.properties.address_line2);
