550c47f94e57c150905b9a38daa93b9466cb3bfc
florareiro <ceciliaflora@outlook.com.br>
2022-11-21 17:41:51 -0300

    clean db

--- public/logs/logs.js
+++ public/logs/logs.js
@@ -1,21 +1,21 @@
 async function getData() {
-    const response = await fetch( '/api');
-    const data = await response.json();
+  const response = await fetch("/api");
+  const data = await response.json();
 
-    for (item of data) {
-        const root = document.createElement('p');
-        const geo = document.createElement('div');
-        const mood = document.createElement('div');
-        const date = document.createElement('div');
-        
+  for (item of data) {
+    const root = document.createElement("p");
+    const geo = document.createElement("div");
+    const mood = document.createElement("div");
+    const date = document.createElement("div");
+    const image = document.createElement("img");
 
-        geo.textContent = `Localização: ${item.cidade}`;
-        mood.textContent = `Anotações: ${item.mood}`;
-        date.textContent = new Date (item.timestamp).toLocaleDateString();
-
-        root.append(geo, mood, date);
-        document.body.append(root);
-    }
-    console.log(data); 
+    geo.textContent = `Localização: ${item.cidade}`;
+    mood.textContent = `Anotações: ${item.mood}`;
+    date.textContent = new Date(item.timestamp).toLocaleDateString();
+    image.src = item.image64;
+    root.append(geo, mood, date, image);
+    document.body.append(root);
+  }
+  console.log(data);
 }
 getData();
--- public/main.js
+++ public/main.js
@@ -1,62 +1,63 @@
+//json com dados do cliente
 
-     //json com dados do cliente
-     let cidade;
-     const button = document.getElementById('submit');
-     button.addEventListener('click', async event => {
-       const mood = document.getElementById('mood').value; 
-       const cidade = document.getElementById('cidade').textContent;
-       const data = {  mood, cidade };
-       const options = {
-         method: 'POST',
-         headers: {
-           'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(data)
-       };
-       const response = await fetch('/api', options);
-       const json = await response.json();
-       console.log(json);
-     });
-         //INICIANDO O MAPA
-         const map = L.map('mapa').setView([0,0], 3);
+function setup() {
+  noCanvas();
+  const video = createCapture(VIDEO);
+  video.size(320, 120);
+  let cidade;
+  const button = document.getElementById("submit");
+  button.addEventListener("click", async (event) => {
+    const mood = document.getElementById("mood").value;
+    const cidade = document.getElementById("cidade").textContent;
+    video.loadPixels();
+    const image64 = video.canvas.toDataURL();
+    const data = { mood, cidade, image64 };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    const response = await fetch("/api", options);
+    const json = await response.json();
+    console.log(json);
+  });
+}
+//INICIANDO O MAPA
+const map = L.map("mapa").setView([0, 0], 3);
 
-const marker =   L.marker([0, 0]).addTo(map);
+const marker = L.marker([0, 0]).addTo(map);
 
 const attribution =
-    '© OpenStreetMap; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';    
-const tiles = L.tileLayer(tileUrl, {attribution});
-tiles.addTo(map);   
+  '© OpenStreetMap; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attribution });
+tiles.addTo(map);
 
 //API DE GEOLOCALIZAÇÃO CLIENTE
 
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition( async position => {
-    console.log('geolocalização disponivel');
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    console.log("geolocalização disponivel");
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    
-    
+
     // console.log(position);
     marker.setLatLng([lat, lon]);
-    console.log(lat,lon);
-    map.setView([lat,lon], 12);   
-   
+    console.log(lat, lon);
+    map.setView([lat, lon], 12);
 
     // API CLIMA
-    const API_key = '32ae98f7cb98e2b734b003cea7eb0273';
+    const API_key = "32ae98f7cb98e2b734b003cea7eb0273";
     const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
     const response = await fetch(api_url);
     const json = await response.json();
-    
+
     //  console.log(json);
-    document.getElementById('cidade').textContent = json.name;
-    const cidade = (json.name);
-    
+    document.getElementById("cidade").textContent = json.name;
+    const cidade = json.name;
   });
-
 } else {
-  console.log('geolocalização não encontrada');
+  console.log("geolocalização não encontrada");
 }
-
-
